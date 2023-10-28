f7e4c3e544442d785167d76aa497b78a7efe031f
thibautxr <texierthibaut2@gmail.com>
2023-10-29 00:23:57 +0200

    add_realisateur() fixed

--- project.py
+++ project.py
@@ -1,5 +1,3 @@
-#TO FIX : DANS add_realisateur LA NATIONALITE EST PAS INSEREE EN ID MAIS DIRECTEMENT EN CHAINE
-
 import sqlite3
 database = "Film_realisateur_genre_nationalite.db"
  
@@ -50,7 +48,14 @@ def add_realisateur():
  
     req = 'INSERT INTO realisateur (nom_realisateur, prenom_realisateur, ddn_realisateur, id_nationalite_realisateur) VALUES (?, ?, ?, ?)'
     data = []
-    a = ['Entrez le nom du réalisateur : ', 'Entrez le prénom du réalisateur : ', 'Entrez la ddn du réalisateur (aaaa-mm-jj) : ', 'Entrez la nationalité du realisateur (FR, US, GB) : '] #liste de tous les prompts affichés à l'écran
+    curseur.execute('SELECT * FROM nationalite')
+    t = curseur.fetchall()
+    temp = ''
+    for i in t:
+        temp = temp + i[1] + ", "
+    a = ['Entrez le nom du réalisateur : ', 'Entrez le prénom du réalisateur : ', 'Entrez la ddn du réalisateur (aaaa-mm-jj) : ', 'Entrez la nationalité du realisateur ($) : '] #liste de tous les prompts affichés à l'écran
+    for i in range(len(a)):
+        a[i] = a[i].replace('$', temp[:-2])
     for i in range(len(a)):                                 # Pour afficher tous les prompt du tableau a[]
         data.append(str(input(a[i])))                       # Ici on ajoute toutes les réponses de l'utilisateur au tableau data[] qui remplaceront les '?' de la requête VALUES (?, ?, ?, ?)
         if i == len(a) - 1:                                 # Quand on a completé tous les champs
@@ -58,13 +63,16 @@ def add_realisateur():
             b = curseur.fetchall()                          # La variable contient les valeurs de la table nationalité, elle est sous la forme : [[id, nationalite], [id, nationalite], ...]
             IsFound = False
             for c in range(len(b)):
-                if data[len(a) - 1].upper() == b[c][1]: # Si la fin du tableau data[], qui contient la nationalité, est identique à l'une des valeurs de la table nationalité (le tableau ressemble à [id, nationalite], [id, nationalite], ...)
+                if data[len(a) - 1].upper() == b[c][1]:     # Si la fin du tableau data[], qui contient la nationalité, est identique à l'une des valeurs de la table nationalité (le tableau ressemble à [id, nationalite], [id, nationalite], ...)
                     data[len(a) - 1] = b[c][0]              # Alors on affecte à la fin du tableau l'id de la nationalite indiquée par l'utilisateur
                     IsFound = True
                     break
             if not IsFound:                                 # Si la nationalite n'est pas dans la table    
                 curseur.execute("INSERT INTO nationalite (nom_nationalite) VALUES ('{}')".format(data[len(a) - 1]))     # Créer une nouvelle nationalité sachant que data[len(a) - 1] contient la nationalité entree par l'utilisateur
+                data[len(a) - 1] = len(t) + 1
 
+    print(req, data)
+    input()
     curseur.execute(req, data)
     curseur.execute("SELECT * FROM realisateur")
     temp = curseur.fetchall()
@@ -167,3 +175,5 @@ def add_film():
     curseur.close()
     connexion.commit()
     connexion.close()
+
+add_realisateur()
